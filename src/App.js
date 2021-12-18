f6e268e95ea23776237c454a9ad29efc6cd91fa9
SEML <4040591@gmail.com>
2021-12-18 22:25:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Form,
+} from "react-bootstrap";
 
 function App() {
+  let [booksFound, setBooksFound] = useState("");
+
+  let [userRequest, setUserRequest] = useState("");
+
+  const projectAPI = "AIzaSyBegn1BYkKYId9tsTKsCtjKa1IhDsFK3JM";
+
+  async function searchForBooks(request) {
+    let first = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${request}&key=${projectAPI}`
+    );
+    let res = await first.json();
+    console.log(res);
+  }
+
+  function submitRequest(event) {
+    event.preventDefault();
+    if (userRequest) {
+      searchForBooks(userRequest);
+    }
+  }
+
+  function trackRequest(event) {
+    let value = event.target.value;
+    setUserRequest([value]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Container>
+        <Form
+          onSubmit={(e) => {
+            submitRequest(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <InputGroup
+            size="sm"
+            className="mb-3"
+            onChange={(e) => trackRequest(e)}
+          >
+            <FormControl
+              placeholder="Please, enter the name of the book"
+              aria-label="Small"
+              aria-describedby="inputGroup-sizing-sm"
+            />
+          </InputGroup>
+          <Button type="submit">Submit</Button>
+        </Form>
+        {/* {if()} */}
+      </Container>
+    </>
   );
 }
 
